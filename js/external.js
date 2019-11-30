8b9e51923c172b387b9c944ac4e3c3f38877c18b
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2019-11-29 23:37:18 -0600

    worked on javascript lesson in html

---
+++ js/external.js
@@ -0,0 +1,51 @@
+// console.log("Hello from external JavaScript");
+
+// alert('Welcome to my Website!');
+// var favColor = prompt("What is your favorite color?");
+// console.log('The user entered: ' + favColor);
+// alert("Great, " + favColor + " is my favorite color too!");
+
+// You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear
+// (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to
+// like it). If price for a movie per day is $3, how much will you have to pay?
+
+// var littleMermaidDays = Number(prompt('You have selected "The Little Mermaid" to rent at a price of $3 a day, how many days would you like to rent this movie?'));
+
+// console.log("user entered " + littleMermaidDays);
+
+// var brotherBearDays = Number(prompt('You have selected "Brother Bear" to rent at a price of $3 a day, how many days would you like to rent this movie?'));
+
+// console.log("user entered " + brotherBearDays);
+
+// var herculesDays = Number(prompt('You have selected "Hercules" to rent at a price of $3 a day, how many days would you like to rent this movie?'));
+
+// console.log("user entered " + herculesDays);
+
+// var totalCost = (herculesDays + brotherBearDays + littleMermaidDays) * 3;
+
+// console.log(totalCost);
+
+// alert('Your total cost is $' + totalCost + ' for selected items.');
+//
+// var googleHours = Number(prompt('How many hours did you work for Google this week?'));
+// var googlePay =  Number(prompt('How much do you make an hour for Google?'));
+// var amazonHours = Number(prompt('How many hours did you work for Amazon this week?'));
+// var amazonPay = Number(prompt('How much do you make an hour for Amazon?'));
+// var facebookHours =  Number(prompt('How many hours did you work for Facebook this week?'));
+// var facebookPay = Number(prompt('How much do you make an hour for Facebook?'));
+// var totalPayCheck = (googleHours * googlePay) + (amazonHours * amazonPay) + (facebookHours * facebookPay);
+// alert('Your paycheck before taxes is $' + totalPayCheck);
+
+
+
+// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+
+var hasCLass = confirm("If class is not full, select 'OK' to continue");
+if () {
+
+} else {
+
+}
+
+var scheduleConflict = confirm("Does new class occur during another pre-enrolled class time? Select 'OK' if there is no existing class during this time, to finalize enrollment.");
+var success = confirm('Success! You have successfully enrolled!');
