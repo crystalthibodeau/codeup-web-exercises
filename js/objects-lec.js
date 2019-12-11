790467b77ac1001c59b6ee9398b5108483daef64
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2019-12-11 14:12:02 -0600

    started objects walk through

--- js/objects-lec.js
+++ js/objects-lec.js
@@ -25,36 +25,124 @@ var car = {
         }
     }
 };
+//
+// console.log("car was made in "+ car.year);
+// console.log("car has the following options:" + car.options);
+// car.options.forEach(function(option){
+//     console.log(option)
+// });
+// car.carGoGoGo("key");
+//
+//
+//
+// var myCar = {
+//     make : "Kia",
+//     model : "Sportage",
+//     year : "2012",
+//     stereo : function(on){
+//         if (on === "on"){
+//             console.log("Max Volume On");
+//         }else {
+//             console.log("bad day, eh?.. Music off");
+//         }
+//     },
+//     options : ["A snazzy steering wheel cover", "AWD", "an amazing turn radius"],
+//     isReliable : true,
+//     comfort: function(option){
+//         if(myCar.options.includes(option))
+//             console.log("Yes, it has " + option + "!");
+//         else
+//             console.log("Hmm not sure on that one ");
+//     }
+//
+// };
+//
+// console.log(myCar.make);
+// console.log(myCar.model);
+// console.log(myCar.make, myCar.model, myCar.year);
+// myCar.options.forEach(function(option){
+//     console.log(option);
+// });
+// myCar.stereo("on");
+// myCar.comfort("sunroof");
+//
+//
+// var truck = new Object();
+// truck.make = "Tesla";
+// truck.model = "CyberTruck";
+// truck.year = 2022;
+// // truck.GoGoG = function(){
+// //     console.log("VROOM");
+// // };
+//
+// console.log(truck);
+//
+// console.log(truck['model']);
 
-console.log("car was made in "+ car.year);
-console.log("car has the following options:" + car.options);
-car.options.forEach(function(option){
-    console.log(option)
-});
-car.carGoGoGo("key");
-
-
-
-var myCar = {
-    make : "Kia",
-    model : "Sportage",
-    year : "2012",
-    stereo : function(on){
-        if (on === "on"){
-            console.log("Max Volume On");
-        }else {
-            console.log("bad day, eh?.. Music off");
+var cars = [
+    car,
+    {
+        make : "Dodge",
+        model: "Challenger",
+        year: 2015,
+        report: function(){
+            console.log(this.year + ' ' + this.make + " " + this.model);
         }
     },
-    options : ["Snazzy steering wheel cover", "AWD", "Amazing turn radius"],
-    isReliable : true,
+    {
+        make: "Ford",
+        model : "Shelby GT 500",
+        year : "1969",
+        report: function(){
+            console.log(this.year + ' ' + this.make + " " + this.model);
+        }
+    },
+    {
+       make : "Cadillac",
+       model: "Escalade",
+       year : "2019",
+        report: function(){
+           console.log(this.year + ' ' + this.make + " " + this.model);
+        }
+    }
 
+];
+car.report = function () {
+    console.log(this.year + ' ' + this.make + " " + this.model);
 };
-
-console.log(myCar.make);
-console.log(myCar.model);
-console.log(myCar.make, myCar.model, myCar.year);
-myCar.options.forEach(function(option){
-    console.log(option);
+cars.forEach(function(car){
+    car.report();
 });
-myCar.stereo("on");
+
+// var dogs = [
+//     {
+//         breed: "Beagle",
+//         bark: "Rooawf, Rooawf, Rooawf!!",
+//         size: "Medium",
+//         report: function(){
+//             console.log(this.breed + ' : ' + this.bark + ' : ' + this.size);
+//         }
+// },
+//     {breed: "Black Lab",
+//         bark: "Rooof, Rooof",
+//         size: "Medium to Large",
+//         report: function(){
+//             console.log(this.breed + ' : ' + this.bark + ' : ' + this.size);
+//         }
+//     },
+//
+//     {breed: "Doberman",
+//         bark: "Arrh, Arrh!",
+//         size: "Large",
+//         report: function(){
+//             console.log(this.breed + ' : ' + this.bark + ' : ' + this.size);
+//         }
+//     },
+// ];
+//
+// dogs.forEach(function(dog){
+//     dog.report();
+// });
+// dogs.report = function () {
+//     console.log(this.breed + ' : ' + this.bark + " : " + this.size);
+// };
---
+++ js/objects.js
@@ -0,0 +1,175 @@
+(function() {
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an object with firstName and lastName properties that are strings
+     * with your first and last name. Store this object in a variable named
+     * `person`.
+     *
+     * Example:
+     *  > console.log(person.firstName) // "Rick"
+     *  > console.log(person.lastName) // "Sanchez"
+     */
+// var person = {
+//         firstName: "Crystal",
+//         lastName: "Thibodeau",
+//     };
+//
+//     console.log(person.firstName, person.lastName);
+    /**
+     * TODO:
+     * Add a sayHello method to the person object that returns a greeting using
+     * the firstName and lastName properties.
+     * console.log the returned message to check your work
+     *
+     * Example
+     * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
+     */
+    // person.sayHello = function sayHello(){
+    //
+    //      return ("Hello from " + person.firstName + " " + person.lastName + "!");
+    // };
+    // console.log(person.sayHello());
+    /** TODO:
+     * HEB has an offer for the shoppers that buy products amounting to
+     * more than $200. If a shopper spends more than $200, they get a 12%
+     * discount. Write a JS program, using conditionals, that logs to the
+     * browser, how much Ryan, Cameron and George need to pay. We know that
+     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
+     * display a line with the name of the person, the amount before the
+     * discount, the discount, if any, and the amount after the discount.
+     *
+     * Uncomment the lines below to create an array of objects where each object
+     * represents one shopper. Use a foreach loop to iterate through the array,
+     * and console.log the relevant messages for each person
+     */
+
+
+    console.log("HEB has an offer for the shoppers that buy products amounting to more than $200. If a shopper spends more than $200, they get a 12% discount. Write a JS program, using conditionals, that logs to the browser, how much Ryan, Cameron and George need to pay. We know that Cameron bought $180, Ryan $250 and George $320. Your program will have to display a line with the name of the person, the amount before the discount, the discount, if any, and the amount after the discount.");
+    console.log("");
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320},
+    ];
+shoppers.forEach(function(shopper) {
+    if (shopper.amount > 200) {
+        console.log("");
+        console.log((shopper.name));
+        console.log("$" + (shopper.amount) + " is your price before discount");
+        console.log("$"  + (shopper.amount * .12) + " is your discount amount");
+        console.log("$" + (shopper.amount - (shopper.amount * .12)) + " is your price after!");
+    }else if(shopper.amount < 200){
+        console.log(shopper.name);
+        console.log("$" + (200 - shopper.amount) + (" is how much more you need to spend to qualify for a 12% discount! If you spend $200 you save $24!"));
+    }
+    else {
+        return false;
+    }
+});
+// shoppers.forEach(report);
+
+
+    /** TODO:
+     * Create an array of objects that represent books and store it in a
+     * variable named `books`. Each object should have a title and an author
+     * property. The author property should be an object with properties
+     * `firstName` and `lastName`. Be creative and add at least 5 books to the
+     * array
+     *
+     * Example:
+     * > console.log(books[0].title) // "The Salmon of Doubt"
+     * > console.log(books[0].author.firstName) // "Douglas"
+     * > console.log(books[0].author.lastName) // "Adams"
+     */
+//
+//     var books = [
+//         {
+//             author : {
+//         firstName: "Alan",
+//         lastName: "Watts"
+// },
+//             title : "The Book on The Taboo Against Knowing Who You Are"
+//         },
+//
+//         {
+//             author : [
+//                 {firstName:"Joseph"},
+//                 {lastName:"Murphy"}
+//             ],
+//             title : "The Power Of Your Subconscious Mind"
+//         },
+//
+//         {
+//             author : [
+//                 {firstName:"Danny"},
+//                 {lastName:"Meyer"}
+//             ],
+//             title : "Setting the Table"
+//         },
+//         {
+//             author : [
+//                 {firstName:"Ayn"},
+//                 {lastName:"Rand"}
+//             ],
+//             title : "Atlas Shrugged"
+//         },
+//         {
+//             author : [
+//                 {firstName:"Danny"},
+//                 {lastName:"Meyer"}
+//             ],
+//             title : "Setting the Table"
+//         }
+//     ];
+
+// console.log(books[0].author.firstName);
+//     console.log(books[0].title);
+
+
+    /**
+     * TODO:
+     * Loop through the books array and output the following information about
+     * each book:
+     * - the book number (use the index of the book in the array)
+     * - the book title
+     * - author's full name (first name + last name)
+     *
+     * Example Console Output:
+     *
+     *      Book # 1
+     *      Title: The Salmon of Doubt
+     *      Author: Douglas Adams
+     *      ---
+     *      Book # 2
+     *      Title: Walkaway
+     *      Author: Cory Doctorow
+     *      ---
+     *      Book # 3
+     *      Title: A Brief History of Time
+     *      Author: Stephen Hawking
+     *      ---
+     *      ...
+     */
+
+
+    // for (var i = 0; i < books.length; i++) {
+    //     console.log("Book # " + (i + 1));
+    //     console.log(books[i].title);
+    //     console.log((books[i].author.firstName)+ " " + (books[i].author.lastName));
+    //     //Do something
+    // }
+
+    /**
+     * Bonus:
+     * - Create a function named `createBook` that accepts a title and author
+     *   name and returns a book object with the properties described
+     *   previously. Refactor your code that creates the books array to instead
+     *   use your function.
+     * - Create a function named `showBookInfo` that accepts a book object and
+     *   outputs the information described above. Refactor your loop to use your
+     *   `showBookInfo` function.
+     */
+
+})();
