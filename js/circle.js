0d756436848d6abbdb13fba0ebf53ba77f356f45
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2019-12-12 11:23:14 -0600

    finsihed math walk through

--- js/circle.js
+++ js/circle.js
@@ -5,36 +5,41 @@
     var circle = {
         radius: 3,
 
-        getArea: function () {
+        getArea: function (radius) {
+            var area;
             // TODO: complete this method
             // hint: area = pi * radius^2
-            let area = Math.PI * (3 * 3);
+            return area = Math.PI * (this.radius * this.radius); //Math.pow(this.radius, 2)
 
-            return area; // TODO: return the proper value
+            // return; // TODO: return the proper value
         },
-circle.getArea();
+
         logInfo: function (doRounding) {
             // TODO: complete this method.
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding === true) {
+               console.log("Circle information rounded to the nearest whole number is: " + Math.round(this.getArea())); //this vs circle
+            }
+            else{
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea(this.radius));
+            }
         }
     };
 
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
+
     circle.logInfo(true);
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
+    // console.log("Circle information rounded to the nearest whole number is: ");
     circle.logInfo(true);
 })();
